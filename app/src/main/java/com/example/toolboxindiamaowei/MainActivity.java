3d76eb5467e6bd3c32b2deb0497c496b54630225
indiamaowei7431 <ebzicorn001@gmail.com>
2019-09-16 12:01:10 -0500

    radio buttons!

--- app/src/main/java/com/example/toolboxindiamaowei/MainActivity.java
+++ app/src/main/java/com/example/toolboxindiamaowei/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 public int charmander = 0;
-    public int squrtile = 0;
+    public int squirtle = 0;
     public int bulbasuar = 0;
     public int charmeleon = 0;
     public int ivysaur = 0;
@@ -20,7 +22,7 @@ public int charmander = 0;
     public int charzard = 0;
 
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -29,17 +31,31 @@ public int charmander = 0;
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.move_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(this);
+        spinner.setOnItemSelectedListener(this);
 
 
     }
 
+    public void onClick(View v){
+        RadioGroup rg = findViewById(R.id.radiogroup);
+        int radioButton = rg.getCheckedRadioButtonId();
+        RadioButton rb = rg.findViewById(radioButton);
+        if(rb.getText().equals("cool")){
 
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            squirtle++;
+        }
 
     }
 
 
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
